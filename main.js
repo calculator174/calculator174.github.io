6683b8e6d2816f8a823df0b388679142d2873b32
calculator174 <kotkot75959@gmail.com>
2018-05-28 18:03:55 +0500

    Inital commit

---
+++ main.js
@@ -0,0 +1,25 @@
+﻿function c() {
+    var s = 0;
+    var a = parseInt(document.getElementById('o').value);
+    var b = parseInt(document.getElementById('b').value);
+    var d = document.getElementById('d').value;
+    if (d == "*") {
+        s = a * b;
+        alert(s);
+    }
+    else if (d == "/") {
+        s = a / b;
+        alert(s);
+    }
+    else if (d == "-") {
+        s = a - b;
+        alert(s);
+    }
+    else if (d == "+") {
+        s = a + b;
+        alert(s);
+    }
+    else {
+        alert("error!")
+    }
+} 
